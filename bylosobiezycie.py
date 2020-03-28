23c6d22f877fe98d2e79344a5c57720d15b28d83
Jakub Dakowski <jakubdakowski@gmail.com>
2020-03-28 11:49:28 +0100

    Implemented eating TODO: debug walking on the edge

--- bylosobiezycie.py
+++ bylosobiezycie.py
@@ -28,12 +28,12 @@ class Section(set):
             s.append(g)
         return s
 
-    def __init__(self, parent, _id, min_max_x, min_max_y, elements=[]):
+    def __init__(self, species_list, _id, min_max_x, min_max_y, elements=[]):
         super().__init__(elements)
         assert len(_id) == 2, "Niepoprawne id"
         assert len(min_max_x) == 2, "Niepoprawne wartości min i max x"
         assert len(min_max_y) == 2, "Niepoprawne wartości min i max y"
-        self.parent = parent
+        self.species_list = species_list
         self.x = min_max_x
         self.y = min_max_y
         self.id = _id
@@ -55,7 +55,7 @@ class Section(set):
             pos = {'left': [-1, 0], 'right': [1, 0],
                    'up': [0, 1], 'down': [0, -1]}[pos]
         try:
-            sec = self.parent[self.id[0]+pos[0]][self.id[1]+pos[1]]
+            sec = self.species_list[self.id[0]+pos[0]][self.id[1]+pos[1]]
         except IndexError:
             sec = {}
         return sec
@@ -78,11 +78,16 @@ class Section(set):
 
 class Life(object):
 
-    def __init__(self, x, y, section_list):
+    def __init__(self, species_list, x, y, section_list):
         self.x = x
         self.y = y
+        self.species_list = species_list
         self.section = section_list[x//Section.size][y//Section.size]
         self.section.add(self)
+
+    def die(self):
+        self.section.discard(self)
+        self.species_list.remove(self)
 
 
 class Plant(Life):
@@ -93,14 +98,14 @@ class Plant(Life):
 
 
 class Animal(Life):
-    SIGHT_RADIUS = 6
+    SIGHT_RADIUS = 20
 
-    def __init__(self, x, y, section, speed):
-        super().__init__(x, y, section)
+    def __init__(self, species_list, x, y, section, speed):
+        super().__init__(species_list, x, y, section)
         self.speed = speed
         self.energy = 1
         self.breeding_need = -4
-        self.interest = 0.1
+        self.interest = 0.01
 
     def see(self, obj):
         x = distance(self.x, obj.x)
@@ -109,7 +114,7 @@ class Animal(Life):
         if dist > self.SIGHT_RADIUS:
             return 0
         else:
-            dist += 0.1**3
+            dist += self.interest*(0.1**2)
             return 1/dist
 
     def __str__(self):
@@ -119,9 +124,9 @@ class Animal(Life):
         assert direction[0] + direction[1] <= self.speed, 'Wrong directions'
         a = abs(self.x + direction[0])
         b = abs(self.y + direction[1])
-        if a>=map_end:
+        if a >= map_end:
             a -= a-map_end+1
-        if b>=map_end:
+        if b >= map_end:
             b -= b-map_end+1
         self.x = a
         self.y = b
@@ -150,7 +155,7 @@ class Animal(Life):
             val = self.see(i)
             if val > 0:
                 if isinstance(i, Plant):
-                    val /= self.energy/self.speed
+                    val /= self.energy
                 if isinstance(i, Animal) and self.breeding_need > 0:
                     val *= self.breeding_need
                 if val > self.interest:
@@ -171,16 +176,21 @@ class Animal(Life):
             a = a // 2
             b = b // 2
         return a, b
-    
+
     def random_walk(self):
-        a = randrange(-self.speed,self.speed+1)
-        b = randrange(-self.speed+abs(a),self.speed+1-abs(a))
+        a = randrange(-self.speed, self.speed+1)
+        b = randrange(-self.speed+abs(a), self.speed+1-abs(a))
         return a, b
+
+    def eat(self, obj):
+        self.energy += obj.nutrition
+        print('szamanie')
+        obj.die()
 
 
 if __name__ == "__main__":
     SECTION_AMOUNT = 1
-    PLANT_AMOUNT = 5
+    PLANT_AMOUNT = 20
     ANIMAL_AMOUNT = 1
 
     section_sqrt = int(sqrt(SECTION_AMOUNT))
@@ -191,9 +201,17 @@ if __name__ == "__main__":
     framerate = pg.time.Clock()
 
     search_sectors = Section.genmap(section_sqrt)
-    animals = [Animal(randrange(0, Section.size*section_sqrt), randrange(0, Section.size*section_sqrt), search_sectors, 2) for i in range(ANIMAL_AMOUNT)]
-    plants = [Plant(randrange(0, Section.size*section_sqrt), 
-                    randrange(0, Section.size*section_sqrt), search_sectors) for i in range(PLANT_AMOUNT)]
+    
+    animals = []
+    for i in range(ANIMAL_AMOUNT):
+        animals.append(Animal(animals, randrange(0, Section.size*section_sqrt),
+                              randrange(0, Section.size*section_sqrt), search_sectors, 2))
+        print(animals[-1])
+
+    plants = []
+    for i in range(PLANT_AMOUNT):
+        plants.append(Plant(plants, randrange(0, Section.size*section_sqrt),
+                            randrange(0, Section.size*section_sqrt), search_sectors))
 
     while True:
         for event in pg.event.get():
@@ -208,7 +226,8 @@ if __name__ == "__main__":
             if target == None:
                 d.move(d.random_walk(), Section.size*section_sqrt)
             elif target.x == d.x and target.y == d.y:
-                print('szamanie')
+                if isinstance(target, Plant):
+                    d.eat(target)
             else:
                 d.move(d.whereto(target), Section.size*section_sqrt)
             pg.draw.rect(screen, (255, 255, 255), (2*d.x, 2*d.y, 2, 2), 0)
